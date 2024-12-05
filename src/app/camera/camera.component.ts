78de1115819483a670deea7a13c911620d5121f0
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-12-06 01:59:29 +0200

    a

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../api.service';
@@ -15,39 +15,27 @@ export class CameraComponent implements OnInit {
 	constructor(private apiService: ApiService, private sessionService: SessionService) {}
 
 	async ngOnInit() {
-		console.log(await this.apiService.getSession(this.sessionService.hostID).toPromise())
-
 		this.canvas = document.createElement('canvas'); // Hidden canvas for capturing video frames
 		this.displayImage = document.getElementById('reconstructedFrame') as HTMLImageElement;
 	}
 
   	camIsOn = false;
-  	videoElement!: HTMLVideoElement;
+  	@ViewChild('videoElement') videoElement!: ElementRef;
 	canvas!: HTMLCanvasElement;
 	displayImage!: HTMLImageElement;
 
 	delay: number = 20
 
   	// Toggle camera on/off
-	async camera(videoElement: HTMLVideoElement): Promise<void> {
-    	this.videoElement = videoElement;
-
+	async camera(): Promise<void> {
     	try {
-			const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-
+			// toggle
 			if (this.camIsOn) {
-				// Turn off
-				this.camIsOn = false;
-				this.videoElement.srcObject = null;
-				this.displayImage.style.display = "none";
+				this.turnOffCamera()
 			} else {
-				// Turn on
-				this.camIsOn = true;
-				this.displayImage.style.display = "block";
-				this.videoElement.srcObject = stream;
-				this.videoElement.play();
+				await this.turnOnCamera()
 				while(this.camIsOn) {
-					await this.send(stream);
+					await this.send();
 					await this.receive();
 					await this.sleep(this.delay);
 				}
@@ -57,24 +45,30 @@ export class CameraComponent implements OnInit {
 		}
   	}
 
+	turnOffCamera() {
+		this.camIsOn = false;
+		this.videoElement.nativeElement.srcObject = null;
+		this.displayImage.style.display = "none";
+	}
+
+	async turnOnCamera() {
+		this.camIsOn = true;
+		this.videoElement.nativeElement.style.display = "none"; // don't display the input version of yourself
+		const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+		this.videoElement.nativeElement.srcObject = stream;
+		this.videoElement.nativeElement.play();
+	}
+	
 	// Start capturing and sending video data
-	async send(stream: MediaStream): Promise<void> {
-		// Capture current frame as a base64 string
-		this.canvas.width = this.videoElement.videoWidth;
-		this.canvas.height = this.videoElement.videoHeight;
-		const context = this.canvas.getContext('2d');
-		context?.drawImage(this.videoElement, 0, 0, this.canvas.width, this.canvas.height);
-
-		const frame = this.canvas.toDataURL('image/jpeg'); // Capture frame as Base64
-
+	async send(): Promise<void> {
 		try {
-			// console.log(this.sessionService.hostID, this.sessionService.myID)
+			const frame = this.createVideoFrame()
 			await this.apiService.postVideo(frame, this.sessionService.hostID, this.sessionService.myID).toPromise();
 		} catch (error) {
 			console.error('Error uploading video chunk:', error);
 		}
 	}
-
+	
 	async receive(): Promise<void> {
 		try {
 			// console.log(this.sessionService.hostID, this.sessionService.myID)
@@ -84,11 +78,17 @@ export class CameraComponent implements OnInit {
 			console.error('Error during video reception:', error);
 		}
 	}
-
+	
 	sleep(ms: number): Promise<void> {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
+	
+	// Capture frame as Base64
+	createVideoFrame(): string {
+		this.canvas.width = this.videoElement.nativeElement.videoWidth;
+		this.canvas.height = this.videoElement.nativeElement.videoHeight;
+		const context = this.canvas.getContext('2d');
+		context?.drawImage(this.videoElement.nativeElement, 0, 0, this.canvas.width, this.canvas.height);
+		return this.canvas.toDataURL('image/jpeg');
+	}
 }
-
-
-
