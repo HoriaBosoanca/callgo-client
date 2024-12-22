7a6d5b2cc33c1120dbddd59ca1eb7bf15216abf1
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-12-22 14:26:32 +0200

    multiple row display

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -28,6 +28,9 @@ export class DisplayComponent implements OnInit {
 
 			for(let i = 0; i < this.images.length; ++i) {
 				this.images[i].src = this.frames[i]
+				if(this.images[i].src == 'data:,') {
+					this.images[i].src = '../../assets/images/black.png'
+				}
 			}
 			
 		}, this.sessionService.delay)
@@ -43,19 +46,35 @@ export class DisplayComponent implements OnInit {
 		}
 		this.images = []
 		for(let i = 0; i < amountOfMembers; ++i) {
-			let img = document.createElement('img')
 
-			// Styles
-			img.style.margin = 'auto'
-			img.alt = ''
-			img.style.borderRadius = '1vh'
-			img.style.borderWidth = '1rem'
-			img.style.width = `${100 / this.frames.length}%`
+			let img: HTMLImageElement = document.createElement('img')
+
+			this.applyStyles(img)
 			
 			this.videoBox.nativeElement.appendChild(img)
 			this.images.push(img)
-			// this.videoBox.nativeElement.innerHTML = ''
 			this.correctImagesGenerated = true
 		}
+	}
+	
+	maxParticipantsPerRow: number = 4
+	applyStyles(img: HTMLImageElement) {
+		img.style.margin = 'auto'
+		img.alt = ''
+		img.style.borderWidth = '1rem'
+		
+		if(this.frames.length <= this.maxParticipantsPerRow) {
+			// there is ony 1 row
+			img.style.width = `${100 / this.frames.length}%`
+			img.style.maxHeight = '100%'
+		} else {
+			// there are multiple rows
+			img.style.width = `${100 / this.maxParticipantsPerRow}%`
+			const amountOfRows = this.frames.length / this.maxParticipantsPerRow
+			img.style.maxHeight = `${100 / amountOfRows}%`
+		}
+
+		img.style.objectFit = 'contain'
+		img.style.borderRadius = '1vh'
 	}
 }
